a9dcd5ce8c6b0987bc47597ddd3c04d4e18db186
nepalcodinghouse <nepalcodinghouse@gmail.com>
2025-10-18 11:28:43 +0545

    add

--- vite.config.js
+++ vite.config.js
@@ -5,30 +5,15 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: './', // relative asset paths for Vercel
   server: {
     proxy: {
       '/api': {
         target: 'https://backend-vauju-1.onrender.com',
         changeOrigin: true,
         secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '/api'),
-        configure: (proxy, options) => {
-          proxy.on('error', (err, req, res) => {
-            console.log('proxy error', err);
-          });
-          proxy.on('proxyReq', (proxyReq, req, res) => {
-            console.log('Sending Request to the Target:', req.method, req.url);
-          });
-          proxy.on('proxyRes', (proxyRes, req, res) => {
-            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
-          });
-        },
       },
     },
-    // This is the key part to fix /@:username 404s
-    historyApiFallback: true, // all unknown routes fallback to index.html
+    historyApiFallback: true, // SPA fallback
   },
-  build: {
-    rollupOptions: {},
-  }
 });
